e553bc104c8dbad605e96e220975ad7b8f5aa399
TimotyDarrelDavidson <c14230235@john.petra.ac.id>
2025-10-28 00:02:11 +0700

    navigation from profile to detail

--- app/src/main/java/uts/c14230235/mynavigation/ProfileFragment.kt
+++ app/src/main/java/uts/c14230235/mynavigation/ProfileFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import uts.c14230235.mynavigation.databinding.FragmentProfileBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,6 +18,9 @@ private const val ARG_PARAM2 = "param2"
  * Use the [ProfileFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+
+
+private var binding: FragmentProfileBinding? = null
 class ProfileFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -33,8 +38,24 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+
+        binding!!.buttonOpen.setOnClickListener {
+            findNavController().navigate(
+                R.id.action_profileFragment_to_detailProfileFragment
+            )
+        }
+
+        return binding!!.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 
     companion object {
